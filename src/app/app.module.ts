52a9ea78777ca77502ec1e23ddc0ed0943b98dab
Matteo <matteo.pipitone93@gmail.com>
2019-06-28 00:11:40 +0200

    3hours of work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import {AngularFileUploaderModule} from 'angular-file-uploader';
+import {NgxSpinnerModule} from 'ngx-spinner';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    FormsModule,
+    AngularFileUploaderModule,
+    NgxSpinnerModule,
+    BrowserAnimationsModule
+
+],
   providers: [],
   bootstrap: [AppComponent]
 })
